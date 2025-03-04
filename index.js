b34c051c956657d1d1b066ccec82aba0bfc4b8d6
alex njugi karanja <alex.karanja4@student.moringaschool.com>
2025-03-04 10:16:56 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Alex Karanja';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
